ae390842ec7e3dda7f3ecfd6140a052b518dc8f5
wedgey <desmondpoon5@hotmail.com>
2016-06-23 01:54:18 +0000

    Added Roman Numerals and Coded Modern Method

---
+++ roman_numerals/roman_numerals.rb
@@ -0,0 +1,109 @@
+ROMAN_DICT = {
+	'1': 'I',
+	'5': 'V',
+	'10': 'X',
+	'50': 'L',
+	'100': 'C',
+	'500': 'D',
+	'1000': 'M'
+}
+
+def traditional_to_roman(num)
+	result = ""
+	ROMAN_DICT.to_a.reverse.to_h.each do |key, value|
+		(num / key.to_s.to_i).times do 
+			result << ROMAN_DICT[key]
+		end
+		num %= key.to_s.to_i
+	end
+	result
+end
+
+require 'pry-byebug'
+
+def to_roman(num)
+	result = ""
+	indices = ROMAN_DICT.keys.reverse
+	while num > 0
+		flag = false
+		wrk_dgt = num.to_s[0].ljust(num.to_s.length,'0').to_i
+		ROMAN_DICT.to_a.reverse.to_h.each do |key, value|
+			indices.each do |val|
+				if (key.to_s.to_i - val.to_s.to_i) == wrk_dgt
+					result << ROMAN_DICT[val] << value
+					num -= wrk_dgt
+					flag = true
+					break
+				end
+			end
+			break if flag
+			if not(flag) && (num / key.to_s.to_i) >= 1
+				(num / key.to_s.to_i).times do
+					result << value
+				end
+				num %= key.to_s.to_i
+				break
+			end
+		end
+	end
+	result
+end
+
+
+
+# def to_roman(num)
+# 	result = ""
+# 	indices = ROMAN_DICT.keys.reverse
+# 	binding.pry
+# 	ROMAN_DICT.to_a.reverse.to_h.each do |key, value|
+
+# 		num_dig = num.to_s[0].ljust(num.to_s.length,'0').to_i
+# 		indices.shift
+# 		indices.each_with_index do |val, i|
+# 			if (key.to_s.to_i - val.to_s.to_i) == num_dig
+# 				result << ROMAN_DICT[val] << value
+# 				num -= num_dig
+# 				break
+# 			end
+# 		end
+
+# 		(num / key.to_s.to_i).times do
+# 			result << ROMAN_DICT[key]
+# 		end
+# 		num %= key.to_s.to_i
+# 	end
+# 	result
+# end
+
+# def to_roman(num)
+# 	result = ""
+# 	index = ROMAN_DICT.keys.reverse
+# 	ROMAN_DICT.to_a.reverse.to_h.each do |key, value|
+# 		if(index[0].to_s.to_i - key.to_s.to_i <= num)
+# 			(num / key.to_s.to_i).times do
+# 				result << ROMAN_DICT[key]
+# 			end
+# 		end
+# 		num %= key.to_s.to_i
+# 		index.shift
+# 		index.each do |val|
+# 			if (key.to_s.to_i - val.to_s.to_i) == (num / val.to_s.to_i)
+# 				result << ROMAN_DICT[val.to_sym] << ROMAN_DICT[key]
+# 				num -= (key.to_s.to_i + val.to_s.to_i) 
+# 				break
+# 			end
+# 		end
+# 	end
+# 	result
+# end
+ 
+# Drive code... this should print out trues.
+ 
+puts to_roman(1) == "I"
+puts to_roman(3) == "III"
+puts to_roman(6) == "VI"
+
+#binding.pry
+puts to_roman(444)
+ 
+# TODO: what other cases could you add to ensure your to_roman method is working?
